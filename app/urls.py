e5267cd78975f16c66ae477779e34e4223d5184d
RuslanAl1mov <2001.ruslan.alimov@gmail.com>
2024-02-12 12:42:48 +0500

    Add link to see results

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.feedback_home_page, name='feedback_home_page'),
-    path('results/', views.feedback_results_page, name='feedback_results_page'),
+    path('jajd2A86jadJF76eO4EpMCNSjak321dan2/results/', views.feedback_results_page, name='feedback_results_page'),
 ]
 
 if settings.DEBUG:
