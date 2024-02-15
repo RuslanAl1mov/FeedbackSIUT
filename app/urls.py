e3ada492538cfbbb4a7a147af68a324474a09d40
RuslanAl1mov <2001.ruslan.alimov@gmail.com>
2024-02-15 12:19:07 +0500

    Create function to print report for feedback

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.feedback_home_page, name='feedback_home_page'),
     path('jajd2A86jadJF76eO4EpMCNSjak321dan2/results/', views.feedback_results_page, name='feedback_results_page'),
+    path('jajd2A86jadJF76eO4EpMCNSjak321dan2/download_report/<int:teacher_id>', views.download_person_feedback_report,
+         name='download_person_feedback_report'),
 ]
 
 if settings.DEBUG:
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import Font, Alignment, PatternFill, Border, Side
+
+from django.http import HttpResponse
+
 
 def feedback_home_page(request):
     if request.method == "POST":
@@ -40,7 +46,7 @@ def feedback_results_page(request):
                        feedback.teacher not in existed_teachers and not existed_teachers.add(feedback.teacher)]
 
     for teacher in unique_teachers:
-        teacher_info = {'name': teacher, 'feedbacks': []}
+        teacher_info = {'teacher': teacher, 'feedbacks': []}
         one_teacher_feedbacks = feedbacks.filter(teacher=teacher)
 
         existed_departments = set()
@@ -95,5 +101,165 @@ def feedback_results_page(request):
             teacher_info['feedbacks'].append(department_feedbacks)
 
         context['teachers'].append(teacher_info)
+        print(context)
 
     return render(request, 'feedback_results.html', context=context)
+
+
+def download_person_feedback_report(request, teacher_id):
+    teacher = get_object_or_404(Teacher, pk=teacher_id)
+    feedbacks = Feedback.objects.select_related('department', 'subject').filter(teacher=teacher)
+
+
+
+    context = {'teacher_info': teacher, 'feedbacks': []}
+
+    existed_departments = set()
+    unique_departments = [feedback.department for feedback in feedbacks if
+                          feedback.department not in existed_departments and not existed_departments.add(
+                              feedback.department)]
+
+    for department in unique_departments:
+        department_feedbacks = {'department': department, 'subjects': [], 'feedbacks_number': 0}
+        department_feedbacks_for_teacher = feedbacks.filter(department=department)
+
+
+        existed_subjects = set()
+        unique_subjects = [feedback.subject for feedback in department_feedbacks_for_teacher if
+                           feedback.subject not in existed_subjects and not existed_subjects.add(feedback.subject)]
+
+        for subject in unique_subjects:
+            feedbacks_for_subject = department_feedbacks_for_teacher.filter(subject=subject)
+            subject_feedbacks = {'subject': subject, 'answers': [], 'total_hor': []}
+
+            for question_num in range(6):  # Предполагается, что вопросов ровно 6
+                answers = []
+                total_vert = []
+                for feedback in feedbacks_for_subject:
+                    answer_obj = list(Answer.objects.filter(feedback=feedback).exclude(question_id__in=[7, 8, 9])
+                                      .values('question_id', 'answer_value')
+                                      .order_by('question_id'))
+
+                    if answer_obj:  # Добавлено условие для проверки существования ответов
+                        answers.append(answer_obj[question_num])
+                        total_vert.append(int(answer_obj[question_num]['answer_value']))
+                        if question_num == 0:
+                            th = sum(int(i['answer_value']) for i in answer_obj)
+                            subject_feedbacks['total_hor'].append(format(th / len(answer_obj), '.2f'))
+                if total_vert:  # Проверка, чтобы избежать деления на ноль
+                    tv = sum(total_vert) / len(total_vert)
+                    answers.append("{:.2f}".format(tv))
+                    total_vert.clear()
+                    subject_feedbacks['answers'].append(answers)
+
+            if subject_feedbacks['total_hor']:  # Проверка, чтобы избежать деления на ноль
+                th = sum(float(i) for i in subject_feedbacks['total_hor']) / len(subject_feedbacks['total_hor'])
+                subject_feedbacks['final_score'] = "{:.2f}".format(th)
+
+            department_feedbacks['subjects'].append(subject_feedbacks)
+
+        context['feedbacks'].append(department_feedbacks)
+
+    wb = Workbook()
+    wb.remove(wb.active)  # Удаляем дефолтный лист
+
+    thin_border = Border(left=Side(style='thin', color="DDDDDD"),
+                         right=Side(style='thin', color="DDDDDD"),
+                         top=Side(style='thin', color="DDDDDD"),
+                         bottom=Side(style='thin', color="DDDDDD"))
+
+    # Создание листа для каждого отделения
+    for department_feedback in context['feedbacks']:
+        ws = wb.create_sheet(title=department_feedback['department'].name[:31])  # Ограничиваем имя листа до 31 символа
+
+        for c in range(20):
+            column_letter = get_column_letter(c+1)
+            ws.column_dimensions[column_letter].width = 15
+
+        for r in range(60):
+            ws.row_dimensions[r].height = 20
+
+        for subject in department_feedback['subjects']:
+            sb_name_row = [subject['subject'].name]
+            ws.append(sb_name_row)
+
+            last_row = ws.max_row
+
+            for col in range(1, len(sb_name_row) + 1):
+                cell = ws.cell(row=last_row, column=col)
+                cell.font = Font(bold=True, size=18)
+
+            header_added = False
+            question_num = 0
+            for answers_list in subject['answers']:
+                if not header_added:
+                    table_headers = [f"Feedback {answer + 1}" for answer in range(len(answers_list))]
+                    table_headers[-1] = "Total:"
+                    table_headers.insert(0, '/')
+                    ws.append([""])
+                    ws.append(table_headers)
+                    last_row = ws.max_row
+
+                    for col in range(1, len(table_headers) + 1):
+                        cell = ws.cell(row=last_row, column=col)
+                        cell.font = Font(bold=True)
+                        cell.alignment = Alignment(horizontal="center")
+                        cell.fill = PatternFill(fill_type="solid", start_color="F2F2F2", end_color="F2F2F2")
+                        cell.border = thin_border
+                    header_added = True
+
+                question_num += 1
+                updated_an_list = [answer['answer_value'] for answer in answers_list if str(type(answer)) == "<class 'dict'>"]
+                updated_an_list.append(answers_list[-1])
+                updated_an_list.insert(0, f'Question {question_num}')
+                ws.append(updated_an_list)
+                last_row = ws.max_row
+
+                for col in range(1, len(updated_an_list) + 1):
+                    cell = ws.cell(row=last_row, column=col)
+                    cell.alignment = Alignment(horizontal="center")
+                    cell.border = thin_border
+                cell = ws.cell(row=last_row, column=1)
+                cell.font = Font(bold=True)
+                cell.fill = PatternFill(fill_type="solid", start_color="F2F2F2", end_color="F2F2F2")
+                cell.border = thin_border
+                cell = ws.cell(row=last_row, column=len(updated_an_list))
+                cell.font = Font(bold=True)
+                cell.fill = PatternFill(fill_type="solid", start_color="F2F2F2", end_color="F2F2F2")
+                cell.border = thin_border
+
+            subject['total_hor'].insert(0, 'Average:')
+            subject['total_hor'].append(subject['final_score'])
+            ws.append(subject['total_hor'])
+            last_row = ws.max_row
+
+            for col in range(1, len(subject['total_hor']) + 1):
+                cell = ws.cell(row=last_row, column=col)
+                cell.font = Font(bold=True)
+                cell.alignment = Alignment(horizontal="center")
+                cell.fill = PatternFill(fill_type="solid", start_color="F2F2F2", end_color="F2F2F2")
+                cell.border = thin_border
+
+            ws.append([""])
+            total_info = [f"Total: {subject['final_score']}"]
+            ws.append(total_info)
+            last_row = ws.max_row
+            cell = ws.cell(row=last_row, column=1)
+            cell.font = Font(bold=True, size=15)
+            cell.alignment = Alignment(horizontal="center")
+            cell.fill = PatternFill(fill_type="solid", start_color="9FF5D7", end_color="9FF5D7")
+            cell.border = thin_border
+
+            ws.append([""])
+            ws.append([""])
+            ws.append([""])
+
+    # Подготовка HttpResponse для скачивания файла
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = f'attachment; filename="{teacher.name}_feedback_report.xlsx"'
+    wb.save(response)
+
+    return response
+
+
+
