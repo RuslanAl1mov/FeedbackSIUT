e713126833c978f5d9eec08e931a7f3e20e6c2e9
RuslanAl1mov <2001.ruslan.alimov@gmail.com>
2024-02-20 10:19:06 +0500

    Change Report download variant.
    Now user will download PDF format of his report.

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
+import os
+import requests
+
 from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+
 from .models import *
 
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font, Alignment, PatternFill, Border, Side
+
+import convertapi
 
 from django.http import HttpResponse
 
@@ -80,7 +87,7 @@ def feedback_results_page(request):
                             th = 0
                             for i in answer_obj:
                                 th += int(i['answer_value'])
-                            subject_feedbacks['total_hor'].append(format(th/len(answer_obj), '.2f'))
+                            subject_feedbacks['total_hor'].append(format(th / len(answer_obj), '.2f'))
                     tv = 0
                     for i in total_vert:
                         tv += i
@@ -106,11 +113,9 @@ def feedback_results_page(request):
     return render(request, 'feedback_results.html', context=context)
 
 
-def download_person_feedback_report(request, teacher_id):
+def download_person_feedback_report_OLD_VERSION(request, teacher_id):
     teacher = get_object_or_404(Teacher, pk=teacher_id)
     feedbacks = Feedback.objects.select_related('department', 'subject').filter(teacher=teacher)
-
-
 
     context = {'teacher_info': teacher, 'feedbacks': []}
 
@@ -122,7 +127,6 @@ def download_person_feedback_report(request, teacher_id):
     for department in unique_departments:
         department_feedbacks = {'department': department, 'subjects': [], 'feedbacks_number': 0}
         department_feedbacks_for_teacher = feedbacks.filter(department=department)
-
 
         existed_subjects = set()
         unique_subjects = [feedback.subject for feedback in department_feedbacks_for_teacher if
@@ -173,7 +177,7 @@ def download_person_feedback_report(request, teacher_id):
         ws = wb.create_sheet(title=department_feedback['department'].name[:31])  # Ограничиваем имя листа до 31 символа
 
         for c in range(20):
-            column_letter = get_column_letter(c+1)
+            column_letter = get_column_letter(c + 1)
             ws.column_dimensions[column_letter].width = 15
 
         for r in range(60):
@@ -209,7 +213,8 @@ def download_person_feedback_report(request, teacher_id):
                     header_added = True
 
                 question_num += 1
-                updated_an_list = [answer['answer_value'] for answer in answers_list if str(type(answer)) == "<class 'dict'>"]
+                updated_an_list = [answer['answer_value'] for answer in answers_list if
+                                   str(type(answer)) == "<class 'dict'>"]
                 updated_an_list.append(answers_list[-1])
                 updated_an_list.insert(0, f'Question {question_num}')
                 ws.append(updated_an_list)
@@ -262,4 +267,121 @@ def download_person_feedback_report(request, teacher_id):
     return response
 
 
+def download_person_feedback_report(request, teacher_id):
+    teacher = get_object_or_404(Teacher, pk=teacher_id)
+    feedbacks = Feedback.objects.select_related('department', 'subject').filter(teacher=teacher)
 
+    context = {'teacher_info': teacher, 'feedbacks': []}
+
+    existed_departments = set()
+    unique_departments = [feedback.department for feedback in feedbacks if
+                          feedback.department not in existed_departments and not existed_departments.add(
+                              feedback.department)]
+
+    for department in unique_departments:
+        department_feedbacks = {'department': department, 'subjects': [], 'feedbacks_number': 0}
+        department_feedbacks_for_teacher = feedbacks.filter(department=department)
+
+        existed_subjects = set()
+        unique_subjects = [feedback.subject for feedback in department_feedbacks_for_teacher if
+                           feedback.subject not in existed_subjects and not existed_subjects.add(feedback.subject)]
+
+        for subject in unique_subjects:
+            feedbacks_for_subject = department_feedbacks_for_teacher.filter(subject=subject)
+            subject_feedbacks = {'subject': subject, 'answers': [], 'total_hor': []}
+
+            for question_num in range(6):  # Предполагается, что вопросов ровно 6
+                answers = []
+                total_vert = []
+                for feedback in feedbacks_for_subject:
+                    answer_obj = list(Answer.objects.filter(feedback=feedback).exclude(question_id__in=[7, 8, 9])
+                                      .values('question_id', 'answer_value')
+                                      .order_by('question_id'))
+
+                    if answer_obj:  # Добавлено условие для проверки существования ответов
+                        answers.append(answer_obj[question_num])
+                        total_vert.append(int(answer_obj[question_num]['answer_value']))
+                        if question_num == 0:
+                            th = sum(int(i['answer_value']) for i in answer_obj)
+                            subject_feedbacks['total_hor'].append(format(th / len(answer_obj), '.2f'))
+                if total_vert:  # Проверка, чтобы избежать деления на ноль
+                    tv = sum(total_vert) / len(total_vert)
+                    answers.append("{:.2f}".format(tv))
+                    total_vert.clear()
+                    subject_feedbacks['answers'].append(answers)
+
+            if subject_feedbacks['total_hor']:  # Проверка, чтобы избежать деления на ноль
+                th = sum(float(i) for i in subject_feedbacks['total_hor']) / len(subject_feedbacks['total_hor'])
+                subject_feedbacks['final_score'] = "{:.2f}".format(th)
+
+            department_feedbacks['subjects'].append(subject_feedbacks)
+
+        context['feedbacks'].append(department_feedbacks)
+
+    # Построение пути к файлу
+    file_path = os.path.join(settings.BASE_DIR, 'FeedbackSIUT/static/app/files/feedback_report_example.xlsx')
+    excel_save_dir = os.path.join(settings.BASE_DIR, 'FeedbackSIUT/static/app/files/cached.xlsx')
+    pdf_save_dir = os.path.join(settings.BASE_DIR, f'FeedbackSIUT/static/app/files/{teacher.name}_feedback_report.pdf')
+    workbook = load_workbook(file_path)
+    ws = workbook.active
+    ws['B13'] = "Teacher's Name - " + teacher.name
+
+    thin_border = Border(left=Side(style='thin'),
+                         right=Side(style='thin'),
+                         top=Side(style='thin'),
+                         bottom=Side(style='thin'))
+
+    i = 0
+    total_of_total_score = 0
+    for department in context['feedbacks']:
+        for subject in enumerate(department['subjects']):
+            i += 1
+            cell_ref = f'B{i + 14}'
+            ws[cell_ref] = i
+            ws[cell_ref].border = thin_border
+            cell_ref = f'C{i + 14}'
+            ws[cell_ref] = department['department'].name
+            ws[cell_ref].border = thin_border
+            cell_ref = f'D{i + 14}'
+            ws[cell_ref] = subject[1]['subject'].name
+            ws[cell_ref].border = thin_border
+            cell_ref = f'E{i + 14}'
+            ws[cell_ref] = subject[1]['final_score']
+            ws[cell_ref].border = thin_border
+            ws[cell_ref].fill = PatternFill(fill_type="solid", start_color="CCFFCC", end_color="CCFFCC")
+            total_of_total_score += float(subject[1]['final_score'])
+    i += 1
+    cell_ref = f'D{i + 14}'
+    ws[cell_ref] = "Total:"
+    ws[cell_ref].alignment = Alignment(horizontal="right")
+
+    cell_ref = f'E{i + 14}'
+    ws[cell_ref] = total_of_total_score
+
+    workbook.save(filename=excel_save_dir)
+    workbook.close()
+
+    # Пути к файлам с учетом директории проекта и имени учителя
+    excel_file_path = os.path.join(settings.BASE_DIR, 'FeedbackSIUT/static/app/files/cached.xlsx')
+    pdf_file_path = os.path.join(settings.BASE_DIR,
+                                 f'FeedbackSIUT/static/app/files/{teacher.name}_feedback_report.pdf')
+
+    # Ваш секретный ключ API от ConvertAPI
+    convertapi.api_secret = 'API_KEY'
+
+    try:
+        # Конвертация файла
+        result = convertapi.convert('pdf', {'File': excel_file_path}, from_format='xlsx')
+        result.file.save(pdf_file_path)
+
+        # Отправляем PDF файл пользователю
+        with open(pdf_file_path, 'rb') as pdf_file:
+            django_response = HttpResponse(pdf_file.read(), content_type='application/pdf')
+            django_response['Content-Disposition'] = f'attachment; filename="{os.path.basename(pdf_file_path)}"'
+            return django_response
+    except Exception as e:
+        return HttpResponse(f"Ошибка при конвертации файла: {e}")
+    finally:
+        # Удаление PDF файла после отправки
+        if os.path.exists(pdf_file_path):
+            os.remove(pdf_file_path)
